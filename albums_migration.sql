81741de86c7a277110a62f9eb288beca4b6cdc3f
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-09 16:49:29 -0500

    refactored albums a bit

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(20),
-    name VARCHAR(20),
-    release_date DATE,
-    sales FLOAT,
-    genre VARCHAR(20),
+    artist VARCHAR(250) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT(4) UNSIGNED,
+    sales FLOAT(5,2) UNSIGNED,
+    genre VARCHAR(255),
     PRIMARY KEY (id)
 );
