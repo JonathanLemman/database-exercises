c420bfe2f47557555e313e33ca17140c9e2e033b
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-10 11:25:02 -0500

    working on notes for lecture and practicing it

--- books_migration.sql
+++ books_migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE books (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     author VARCHAR(40) NOT NULL DEFAULT 'Unknown',
-    date_published DATE NOT NULL,
+    date_published INT NOT NULL,
     description TEXT,
     bestseller_weeks INT NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
