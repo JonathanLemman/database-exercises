0c4b798c50598576832b61feccd4d7f0ca7f4e1e
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-15 11:20:06 -0500

    finished group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,26 @@ SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 group by last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+group by first_name, last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+group by last_name;
+
+SELECT last_name, count(*)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+group by last_name
+ORDER BY COUNT(*);
+
+SELECT count(*), gender
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya'
+group by gender;
