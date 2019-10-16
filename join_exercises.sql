eee0323d3be60b0b6cd290af4222d16c47e5c8fa
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-16 13:51:03 -0500

    finished second problem on exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -11,3 +11,14 @@ JOIN departments
 ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date > now()
 order by departments.dept_name;
+
+# Find the name of all departments currently managed by women.
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name) as full_name
+FROM employees
+JOIN dept_manager
+ON dept_manager.emp_no = employees.emp_no
+JOIN departments
+ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date > now() AND employees.employees.gender = 'f'
+order by departments.dept_name;
